6c1fb57f03ccab18bde2e7dd7605950b9638c34e
Chenla <chenla24@gmail.com>
2019-07-15 10:23:05 -0400

    heroku first push

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,5 +3,6 @@ const productRoutes = require("./products");
 
 // Product routes
 router.use("/products", productRoutes);
+router.use("/user", productRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build")); }
@@ -14,9 +15,30 @@ if (process.env.NODE_ENV === "production") {
 
 app.use("/", routes);
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/CurbUser");
+const mongoURI = 'mongodb://localhost:27017/CurbUser'
+mongoose
+  .connect(
+    mongoURI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
+
 var Users = require('./routes/Users')
 app.use('/users', Users)
+
+app.post("/submit", function(req, res) {
+  // Create a new user using req.body
+  User.create(req.body)
+    .then(function(dbUser) {
+      // If saved successfully, send the the new User document to the client
+      res.json(dbUser);
+    })
+    .catch(function(err) {
+      // If an error occurs, send the error to the client
+      res.json(err);
+    });
+});
 
 
 // Start the API server
