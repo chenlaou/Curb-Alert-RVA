b452078facc7169b71abdeeceba81feccc8af638
Liron <millhousenm@gmail.com>
2019-07-16 21:20:49 -0400

    styled register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,37 +1,38 @@
-import React, { Component } from 'react'
-import { register } from './UserFunctions'
+import React, { Component } from "react";
+import { register } from "./UserFunctions";
+import "./Register.css";
 
 class Register extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      first_name: '',
-      last_name: '',
-      email: '',
-      password: '',
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
       errors: {}
-    }
+    };
 
-    this.onChange = this.onChange.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onChange(e) {
-    this.setState({ [e.target.name]: e.target.value })
+    this.setState({ [e.target.name]: e.target.value });
   }
   onSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     const newUser = {
       first_name: this.state.first_name,
       last_name: this.state.last_name,
       email: this.state.email,
       password: this.state.password
-    }
+    };
 
     register(newUser).then(res => {
-      this.props.history.push(`/login`)
-    })
+      this.props.history.push(`/login`);
+    });
   }
 
   render() {
@@ -40,42 +41,40 @@ class Register extends Component {
         <div className="row">
           <div className="col-md-6 mt-5 mx-auto">
             <form noValidate onSubmit={this.onSubmit}>
-              <h1 className="h3 mb-3 font-weight-normal">Register</h1>
+              <div classNamw="banner">
+                <h1 className="h3 mb-3 font-weight-normal">Register</h1>
+              </div>
               <div className="form-group">
-                <label htmlFor="name">First name</label>
                 <input
                   type="text"
                   className="form-control"
                   name="first_name"
-                  placeholder="Enter your first name"
+                  placeholder="First Name"
                   value={this.state.first_name}
                   onChange={this.onChange}
                 />
               </div>
               <div className="form-group">
-                <label htmlFor="name">Last name</label>
                 <input
                   type="text"
                   className="form-control"
                   name="last_name"
-                  placeholder="Enter your lastname name"
+                  placeholder="Last Name"
                   value={this.state.last_name}
                   onChange={this.onChange}
                 />
               </div>
               <div className="form-group">
-                <label htmlFor="email">Email address</label>
                 <input
                   type="email"
                   className="form-control"
                   name="email"
-                  placeholder="Enter email"
+                  placeholder="Email address"
                   value={this.state.email}
                   onChange={this.onChange}
                 />
               </div>
               <div className="form-group">
-                <label htmlFor="password">Password</label>
                 <input
                   type="password"
                   className="form-control"
@@ -95,8 +94,8 @@ class Register extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Register
+export default Register;
