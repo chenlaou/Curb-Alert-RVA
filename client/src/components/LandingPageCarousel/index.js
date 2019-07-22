52a057e6c93008184a8d8f6524879f5f809ca601
Chenla <chenla24@gmail.com>
2019-07-22 19:25:41 -0400

    carousel

--- client/src/components/LandingPageCarousel/index.js
+++ client/src/components/LandingPageCarousel/index.js
@@ -1,50 +1,47 @@
-import React, { Component } from "react";
-import Carousel from "react-bootstrap/Carousel";
-
+import React from "react";
+import { Carousel } from "react-bootstrap";
 import "./LandingPageCarousel.css";
+
 
 function LandingPageCarousel() {
   return (
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-60"
-          src="./images/openmind.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-60"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
-          alt="Third slide"
-        />
+    <div className="container">
+<Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://oimg.photobucket.com/albums/v240/Chenla/car3.png?t=1563657862"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h4>This is the place to peruse and post free local items</h4>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://oimg.photobucket.com/albums/v240/Chenla/car2.jpg?t=1563658066"
+      alt="Second slide"
+    />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-60"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
-          alt="Third slide"
-        />
+    <Carousel.Caption>
+      <h4>Help elimnate waste by reducing, reusing, and upcycling.</h4>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://i.imgur.com/lg48n0Y.jpg"
+      alt="Third slide"
+    />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
-}
+    <Carousel.Caption>
+      <h4>Create an account to search and share items!</h4>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+</div>
+
+   ) }
 
 export default LandingPageCarousel;
